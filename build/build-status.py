d86944f8cedeff817a86631daec218e6c7d03952
16CO216 <darshandv10@gmail.com>
2019-04-04 11:10:41 +0530

    Removed a few files

---
+++ build/build-status.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+# Programs that are runnable.
+ns3_runnable_programs = ['build/scratch/ns3.29-three-gpp-http-example-debug', 'build/scratch/ns3.29-scratch-simulator-debug', 'build/scratch/ns3.29-sixth-debug', 'build/scratch/subdir/ns3.29-subdir-debug', 'build/scratch/ns3.29-fifth-debug']
+
+# Scripts that are runnable.
+ns3_runnable_scripts = []
+
